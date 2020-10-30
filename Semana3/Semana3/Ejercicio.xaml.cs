88f255cdcd349d88190161764aa33c263f7ad41f
jmcuenca <juan.cuenca.vega@gmail.com>
2020-10-30 12:55:56 -0500

    Inicial, creación de repositorio

---
+++ Semana3/Semana3/Ejercicio.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana3
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Ejercicio : ContentPage
+	{
+		public Ejercicio ()
+		{
+			InitializeComponent ();
+		}
+        public void btnCalcular_Clicked(object sendr, EventArgs args)
+        {
+            decimal parcial1 = decimal.Parse(txtNota1.Text) * 0.3M + decimal.Parse(txtExamen1.Text) * 0.2M;
+            decimal parcial2 = decimal.Parse(txtNota2.Text) * 0.3M + decimal.Parse(txtExamen2.Text) * 0.2M;
+            decimal total = parcial1 + parcial2;
+            txtResultado.Text = (total > 7) ? "APROBADO" : "REPROBADO";
+
+        }
+    }
+}
---
+++ Semana3/Semana3/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana3
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+			InitializeComponent ();
+		}
+        public  void btnIngresar_Clicked(object sender, EventArgs arg) {
+
+            if (txtClave.Text.Equals("uisrael2020") && txtUsuario.Text.Equals("estudiante2020"))
+            {
+                abrirVentanaAsync();
+
+            }
+            else {
+                DisplayAlert("Error", "Usuario o clave incorrecta", "Error");
+                this.txtClave.Text = "";
+                this.txtUsuario.Text = "";
+            }
+        }
+        private async void abrirVentanaAsync() {
+            Ejercicio ejercicio = new Ejercicio();
+            await this.Navigation.PushAsync(ejercicio);
+        }
+    }
+}
